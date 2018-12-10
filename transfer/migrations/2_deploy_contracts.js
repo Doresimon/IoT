7161b43b97bf8e2948da3a59f530bf367caf7457
Halo Guan <doresimon@163.com>
2018-12-10 23:11:22 +0800

    [clean] old files

--- transfer/migrations/2_deploy_contracts.js
+++
@@ -1,7 +0,0 @@
-var Trans = artifacts.require("./Trans.sol");
-// var Ownable = artifacts.require("./openzeppelin-solidity/contracts/ownership/Ownable.sol");
-
-module.exports = function(deployer) {
-    console.log('[deployer.deploy(Trans)]')
-    deployer.deploy(Trans);
-};
